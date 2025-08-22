7f69964e40db3c9ac2ea4bf5a23bc121908e10d8
Mamunur-Roshid99 <roshidmamunur569@gmail.com>
2025-08-22 23:10:39 +0600

    .

--- src/components/landingPage/VideoSection.jsx
+++ src/components/landingPage/VideoSection.jsx
@@ -1,29 +1,55 @@
-import React from 'react';
+"use client"
+
+import API from "@/app/utils/axios";
+import React, { useEffect, useState } from "react";
 
 const VideoSection = () => {
-    return (
-        <>
-            <div className={"video-section lg:p-3 p-2 lg:mt-20 md:mt-16 mt-8"}>
-                <h2 className={"lg:text-2xl md:text-xl text-lg text-white font-medium text-center"}>
-                    বিস্তারিত ভিডিওতে
-                </h2>
-            </div>
+  const [videoTitle, setVideoTitle] = useState("");
+  const [videoUrl, setVideoUrl] = useState("/video/video.mp4");
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const fetchMajorSection = async () => {
+      try {
+        const res = await API.get("/configurepage/majorsection");
+        setVideoTitle(res.data.videoTitle);
+        setVideoUrl(res.data.videoUrl);
+      } catch (error) {
+        console.error("Failed to fetch footer data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-            <div className={" w-full bg-primary"}>
-                <video
-                    className="w-full h-full"
-                    controls
-                    autoPlay
-                    loop
-                    muted
-                    playsInline
-                >
-                    <source src="/video/video.mp4" />
-                </video>
-            </div>
-        </>
-    );
+    fetchMajorSection();
+  }, []);
+
+  return (
+    <>
+      <div className={"video-section lg:p-3 p-2 lg:mt-20 md:mt-16 mt-8"}>
+        <h2
+          className={
+            "lg:text-2xl md:text-xl text-lg text-white font-medium text-center"
+          }
+        >
+          {loading ? "Loading..." : videoTitle}
+        </h2>
+      </div>
+
+      <div className={" w-full bg-primary"}>
+        <video
+          className="w-full h-full"
+          controls
+          autoPlay
+          loop
+          muted
+          playsInline
+        >
+          <source src={videoUrl} type="video/mp4" />
+        </video>
+      </div>
+    </>
+  );
 };
 
 export default VideoSection;
